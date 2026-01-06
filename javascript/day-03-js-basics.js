801f98f48ce7423d74d4335e1d7df33d55b0a89e
Nitin Ghildiyal <ghildiyalnitin067@gmail.com>
2026-01-06 19:31:47 +0530

    Day 3: JavaScript basics till loops

---
+++ javascript/day-03-js-basics.js
@@ -0,0 +1,27 @@
+// Day 3 - JavaScript Basics (Till Loops)
+
+// Variables
+let name = "Nitin";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// If-else
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log("For loop count:", i);
+}
+
+// While loop
+let j = 1;
+while (j <= 3) {
+  console.log("While loop count:", j);
+  j++;
+}
